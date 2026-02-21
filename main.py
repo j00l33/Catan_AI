600204b853c57553616b53553f5aa5284af6f69b
j00l33 <julietteschiff22@gmail.com>
2026-02-21 16:48:34 -0500

    Clean up comments in main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import random
 import string
 
-#board - hexagon (3x3x3x3x3) - tile (type + number) - peices are placed IN BETWEEN TIELES
-#players - rources - points - owned structures - card 
-#turn - dice (rob) - trade or build or  use card
-#loop - players take turns 
+# Initial fleshing out of the catan game itself prototype
+# Proposed classes: tile, board, player, structure, card 
 
 class tile:
     def __init__(self, resource_type, dice_number, robbed_state):
@@ -18,18 +16,17 @@ class board:
     def __init__(self, tiles):
         self.tiles = tiles
     
-
 def create_board():
     board = []
     for i in range(-2,3):
         board_layer = []
         for j in range (5 - abs(i)):
             board_layer.append(tile(random.choice(string.ascii_lowercase), random.randint(0,4), 0))
-            board_layer[-1].tile_print()
+            board_layer[-1].tile_print()  
         board.append(board_layer)
     return board
 
-the_board = board(create_board())
+Board = board(create_board())
 
 
 
